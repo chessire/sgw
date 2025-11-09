38f185ee4500188d49721920e30c69343208f686
nineright backend <>
2025-11-09 11:39:44 +0900

    취약점 개선

--- common/web/src/main/java/sgw/common/web/config/SecurityConfiguration.java
+++ common/web/src/main/java/sgw/common/web/config/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf()
             .authorizeRequests()
                 .anyRequest().authenticated()
             .and()
